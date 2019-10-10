84b5080536812a9d18ec47ed82fd5fd3a4199a6c
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-10 16:58:58 -0700

    Build based on eclipse-mosquitto docker image.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,6 @@ set -e
 while true; do
   $@ &
   PID=$!
-  inotifywait -e modify -e move -e create -e delete -e attrib -r /mosquitto/config/reload
+  inotifywait -e modify -e move -e create -e delete -e attrib /mosquitto/config/reload
   kill $PID
 done
