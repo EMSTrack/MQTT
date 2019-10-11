3c7c7f5a30bd773e841c63a7711cf819f878f5fc
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-11 09:41:59 -0700

    Cleaning up repo.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ trap sigint_handler SIGINT
 set -e
 touch /mosquitto/config/reload
 while true; do
-  $@ &
+  nohup bash -c "$@> /mosquitto/log/mosquitto.log 2>&1 &" &
   PID=$!
   inotifywait -e modify -e move -e create -e delete -e attrib /mosquitto/config/reload
   kill $PID
