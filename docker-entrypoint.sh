744d9b436e8a6bd3f669060af5a177acc19493cf
Mauricio de Oliveira <mcdeoliveira@earthlink.net>
2019-10-11 20:29:20 -0700

    Sending SIGINT upon reload.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,13 +11,13 @@ trap sigint_handler SIGINT
 set -e
 touch /mosquitto/config/reload
 while true; do
-  echo "Starting mosquitto..."
-  $@ > /mosquitto/log/mosquitto.log 2>&1 &
-  PID=$!
-  echo "Mosquitto started"
-  inotifywait -e modify -e move -e create -e delete -e attrib /mosquitto/config/reload
-  echo "Stopping mosquitto..."
-  kill -s SIGINT $PID
-  echo "Mosquitto stopped"
-  sleep 2
+    echo "Starting mosquitto..."
+    $@ > /mosquitto/log/mosquitto.log 2>&1 &
+    PID=$!
+    echo "Mosquitto started"
+    inotifywait -e modify -e move -e create -e delete -e attrib /mosquitto/config/reload
+    echo "Stopping mosquitto..."
+    kill -s SIGINT $PID
+    echo "Mosquitto stopped"
+    sleep 2
 done
